c91d990796ea623208ac7521c58339d6896e87d7
Nachocho11 <theigragar11@gmail.com>
2018-05-07 23:37:58 +0200

    Add files via upload

--- nuevaguiaverquetal/src/controladores/ControladorOfertas.java
+++ nuevaguiaverquetal/src/controladores/ControladorOfertas.java
@@ -3,12 +3,15 @@ package controladores;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JList;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import app.Sistema;
 import gui.CustomFrame;
+import ofertas.Oferta;
 import paneles.OferPanel;
+import usuarios.Usuario;
 
 public class ControladorOfertas implements ActionListener, ListSelectionListener{
 	private CustomFrame ventana;
@@ -29,10 +32,31 @@ public class ControladorOfertas implements ActionListener, ListSelectionListener
 	public void actionPerformed(ActionEvent arg0) {
 		OferPanel panel = ventana.getPanelOfertas();
 		Object event = arg0.getSource();
+		JList list = panel.getOfers();
 		
 		if(event.equals(panel.getBotonDemandante())) {
 			ventana.getPanelDemandante().iniciar(app);
 			ventana.mostrarPanelDemandante();
+			
+		}else if(event.equals(panel.getBotonVer())) {
+			if(list.isSelectionEmpty() == false) {
+				int index =  list.getSelectedIndex();
+				Oferta o;
+				if((o = (Oferta)list.getModel().getElementAt(index)) == null) {
+					return;
+				}
+				ventana.getPanelVerOferta().iniciar(o);
+				ventana.mostrarPanelVerOferta();
+			}
+			
+		}else if(event.equals(panel.getBotonVolver())) {
+			ventana.mostrarPanelRol();
+			
+		}else if(event.equals(panel.getBotonFiltrar())) {
+			
+			
+		}else if(event.equals(panel.getBotonOrdenar())) {
+			
 		}
 	}
 
---
+++ nuevaguiaverquetal/src/controladores/ControladorVerOferta.java
@@ -0,0 +1,90 @@
+package controladores;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import app.Sistema;
+import es.uam.eps.padsof.telecard.FailedInternetConnectionException;
+import es.uam.eps.padsof.telecard.InvalidCardNumberException;
+import es.uam.eps.padsof.telecard.OrderRejectedException;
+import excepciones.ArgumentoNoValido;
+import excepciones.DemasiadasOfertasReservadas;
+import excepciones.OfertaNoDisponible;
+import excepciones.UsuarioBloqueado;
+import gui.CustomFrame;
+import ofertas.Oferta;
+import paneles.OferPanel;
+import paneles.VerOfertaPanel;
+import usuarios.Demandante;
+import usuarios.Rol;
+
+public class ControladorVerOferta implements ActionListener{
+	private CustomFrame ventana;
+	private Sistema app;
+	
+	public ControladorVerOferta(CustomFrame ventana, Sistema sist) {
+		this.ventana = ventana; 
+		this.app = app;	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Object event = arg0.getSource();
+		VerOfertaPanel panel = ventana.getPanelVerOferta();
+		Oferta o = panel.getOferta();
+		Demandante deman = null;
+		
+		if(event.equals(panel.getBotonContratar())) {
+			
+			for(Rol r: app.getUsuarioActual().getRoles()) {
+				if(r.isDemandante()) {
+					deman = (Demandante)r;
+				}
+			}
+			if(deman.isBloqueado()) {
+				JOptionPane.showMessageDialog(null, "No puedes contratar ofertas estando bloqueado");
+				return;
+			}
+			try {
+				o.pagar(deman, "Pago");
+			} catch (InvalidCardNumberException e) {
+				JOptionPane.showMessageDialog(null, "Número de tarjeta no valido");
+				deman.setBloqueado(true);
+			} catch (FailedInternetConnectionException e) {
+				JOptionPane.showMessageDialog(null, "La conexión a internet ha fallado");
+			} catch (OrderRejectedException e) {
+				JOptionPane.showMessageDialog(null, "La operación ha sido rechazada");
+			} catch (ArgumentoNoValido e) {
+				JOptionPane.showMessageDialog(null, "Oh! parece que ha habido un error :)");
+			}
+		}else if(event.equals(panel.getBotonReservar())) {
+			try {
+				o.reservar(deman, ventana.getFecha());
+			} catch (UsuarioBloqueado e) {
+				JOptionPane.showMessageDialog(null, "No puedes reservar ofertas estando bloqueado");
+			} catch (OfertaNoDisponible e) {
+				JOptionPane.showMessageDialog(null, "Esta oferta no esta disponible para reserva");
+			} catch (DemasiadasOfertasReservadas e) {
+				JOptionPane.showMessageDialog(null, "Tienes demasiadas ofertas reservadas");
+			}
+		}else if(event.equals(panel.getBotonComentar())) {
+			
+		}else if(event.equals(panel.getBotonValorar())) {
+			int val;
+			val = Integer.parseInt(JOptionPane.showInputDialog("Introduzca una valoracion entre 1 y 10"));
+			try {
+				o.valorar(val);
+			} catch (ArgumentoNoValido e) {
+				JOptionPane.showMessageDialog(null, "Error,puntuación inválida");
+			}
+			
+		}else if(event.equals(panel.getBotonVolver())) {
+			OferPanel ofer = ventana.getPanelOfertas();
+			ofer.getOfers().clearSelection();
+			ventana.mostrarPanelOfertas();
+		}
+	}
+
+}
